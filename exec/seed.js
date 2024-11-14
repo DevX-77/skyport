181a4944bc73a9466689c69098b36c676fb09c3f
DevX <kumawatnirmala92@gmail.com>
2024-11-14 19:12:56 +0530

    Update seed.js

--- exec/seed.js
+++ exec/seed.js
@@ -1,9 +1,8 @@
 const axios = require('axios');
 const { db } = require('../handlers/db');
-const log = new (require('cat-loggr'))();
+const CatLoggr = require('cat-loggr');
+const log = new CatLoggr();
 const readline = require('readline');
-const { v4: uuidv4 } = require('uuid');
-const config = require('../config.json');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -12,6 +11,7 @@ const rl = readline.createInterface({
 
 async function seed() {
   try {
+    // First check if there are any images already in the database
     const existingImages = await db.get('images');
     if (existingImages && existingImages.length > 0) {
       rl.question('\'images\' is already set in the database. Do you want to continue seeding? (y/n) ', async (answer) => {
@@ -36,7 +36,7 @@ async function seed() {
 
 async function performSeeding() {
   try {
-    const imagesIndexResponse = await axios.get('https://raw.githubusercontent.com/skyportlabs/images_v2/main/seed/0.1.0-beta2.json');
+    const imagesIndexResponse = await axios.get('https://raw.githubusercontent.com/DevX-77/skyport-images/refs/heads/main/seed/images.json');
     const imageUrls = imagesIndexResponse.data;
     let imageDataArray = [];
 
@@ -44,12 +44,8 @@ async function performSeeding() {
       log.init('fetching image data...');
       try {
         const imageDataResponse = await axios.get(url);
-        let imageData = imageDataResponse.data;
-        imageData.Id = uuidv4();
-
-      
-        log.init('seeding: ' + imageData.Name);
-        imageDataArray.push(imageData);
+        log.init('seeding: ' + imageDataResponse.data.Name);
+        imageDataArray.push(imageDataResponse.data);
       } catch (error) {
         log.error(`failed to fetch image data from ${url}: ${error}`);
       }
